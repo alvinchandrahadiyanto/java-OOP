83f301b84fa3f910e7df2f24e5cc33286a78a39b
alvinchandrahadiyanto <chandrahadiyantoalvin@gmail.com>
2022-12-26 15:17:10 +0700

    Java DAO

---
+++ task3/dao/Main.java
@@ -0,0 +1,54 @@
+package dao;
+
+import dao.controller.BookController;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    static String opsi;
+
+  public static void menuApp() {
+    System.out.println("=== SISTEM BUKU MANAGEMENT ===");
+    System.out.println("1. Lihat Data Buku");
+    System.out.println("2. Cari Data Buku Berdasar ID");
+    System.out.println("3. Tambah Data Buku");
+    System.out.println("4. Edit Data Buku");
+    System.out.println("5. Hapus Data Buku");
+    System.out.print("Pilihan User: ");
+    opsi = input.nextLine();
+  }
+
+  public static void main(String[] args) {
+    do {
+      menuApp();
+
+      switch (opsi) {
+        case "1":
+        
+          break;
+
+        case "2":
+          
+          break;
+
+        case "3":
+
+          break;
+
+        case "4":
+
+          break;
+
+        default:
+          System.out.println("Pilihan tidak tersedia!");
+          break;
+      }
+
+      System.out.print("Apakah ingin keluar aplikasi? (y|n)");
+      opsi = input.nextLine();
+    } while (opsi.equalsIgnoreCase("n"));
+
+    System.out.println("Terima kasih. Sampai jumpa kembali!");
+  }
+}
--- task3/dao/controller/BookController.java
+++ task3/dao/controller/BookController.java
@@ -8,107 +8,62 @@ import java.util.Scanner;
 
 public class BookController {
   private static DaoService<Book, Integer> bookDao = new BookRepository();
-
-  // Instance scanner
+  Book book = new Book();
   static Scanner input = new Scanner(System.in);
   static String opsi;
   static String judul;
   static String pembuat;
   static Integer idbook;
 
-  public static void menuApp() {
-    System.out.println("=== SISTEM BUKU MANAGEMENT ===");
-    System.out.println("1. Tambah Data Buku");
-    System.out.println("2. Lihat Data Buku");
-    System.out.println("3. Hapus Data Buku");
-    System.out.println("4. Edit Data Buku");
-    System.out.println("5. Cari Data Buku Berdasar ID");
-    System.out.print("Pilihan User: ");
-    opsi = input.nextLine();
+  public void getBooks(){
+    System.out.println("Semua Data Buku");
+    System.out.println(bookDao.findAll());
   }
 
-  public static void main(String[] args) {
-    // get all, get by id, delete
-    // instance object book
-    Book book = new Book();
-    book.setTitle("Buku 1");
-    book.setAuthor("Penulis 1");
-    // save data
-    bookDao.save(book);
+  public void getBookById(){
+    System.out.println("Cari Data Buku");
+    System.out.print("Pilihan ID Buku yang akan dicari: ");
+    idbook = Integer.parseInt(input.nextLine());
+    System.out.println(bookDao.findById(idbook));
+  }
 
+  public void saveBook(){
+    System.out.println("Tambah Data Buku");
+    System.out.print("Pilihan Judul Buku: ");
+    judul = input.nextLine();
+    System.out.print("Pilihan Pembuat Buku: ");
+    pembuat = input.nextLine();
     book = new Book();
-    book.setTitle("Buku 2");
-    book.setAuthor("Penulis 2");
+    book.setTitle(judul);
+    book.setAuthor(pembuat);
     // save data
     bookDao.save(book);
+  }
 
-    // getall
+  public void updateBook(){
     System.out.println(bookDao.findAll());
-
-    do {
-      menuApp();
-
-      switch (opsi) {
-        case "1":
-          System.out.println("Tambah Data Buku");
-          System.out.print("Pilihan Judul Buku: ");
-          judul = input.nextLine();
-          System.out.print("Pilihan Pembuat Buku: ");
-          pembuat = input.nextLine();
-          book = new Book();
-          book.setTitle(judul);
-          book.setAuthor(pembuat);
-          // save data
-          bookDao.save(book);
-          break;
-
-        case "2":
-          System.out.println("Semua Data Buku");
-          System.out.println(bookDao.findAll());
-          break;
-
-        case "3":
-          System.out.println(bookDao.findAll());
-          System.out.println("Hapus Data Buku");
-          System.out.print("Pilihan ID Buku yang akan dihapus: ");
-          idbook = input.nextInt();
-          bookDao.delete(idbook);
-          System.out.println(bookDao.findAll());
-          break;
-
-        case "4":
-          System.out.println(bookDao.findAll());
-          System.out.println("Edit Data Buku");
-          System.out.print("Pilihan ID Buku yang akan diedit: ");
-          idbook = Integer.parseInt(input.nextLine());
-          System.out.print("Edit Judul Buku: ");
-          judul = input.nextLine();
-          System.out.print("Edit Pembuat Buku: ");
-          pembuat = input.nextLine();
+    System.out.println("Edit Data Buku");
+    System.out.print("Pilihan ID Buku yang akan diedit: ");
+    idbook = Integer.parseInt(input.nextLine());
+    System.out.print("Edit Judul Buku: ");
+    judul = input.nextLine();
+    System.out.print("Edit Pembuat Buku: ");
+    pembuat = input.nextLine();
           
-          book = new Book();
-          book.setTitle(judul);
-          book.setAuthor(pembuat);
-          // save data
-          bookDao.update(book,idbook);
-          System.out.println(bookDao.findAll());
-          break;
+    book = new Book();
+    book.setTitle(judul);
+    book.setAuthor(pembuat);
+    // save data
+    bookDao.update(book,idbook);
+    System.out.println(bookDao.findAll());
+  }
 
-        case "5":
-          System.out.println(bookDao.findAll());
-          System.out.println("Edit Data Buku");
-          System.out.print("Pilihan ID Buku yang akan dicari: ");
-          idbook = Integer.parseInt(input.nextLine());
-          System.out.println(bookDao.findById(idbook));
-          break;
-
-        default:
-          System.out.println("Pilihan tidak tersedia!");
-          break;
-      }
-
-      System.out.print("Apakah ingin keluar aplikasi? (y|n)");
-      opsi = input.nextLine();
-    } while (opsi.equalsIgnoreCase("n"));
+  public void deleteBook(){
+    System.out.println(bookDao.findAll());
+    System.out.println("Hapus Data Buku");
+    System.out.print("Pilihan ID Buku yang akan dihapus: ");
+    idbook = input.nextInt();
+    bookDao.delete(idbook);
+    System.out.println(bookDao.findAll());
   }
 }
