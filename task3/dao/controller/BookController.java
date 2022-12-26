7fd4409a3128f7d323eff6b8bca3eacbfd076517
alvinchandrahadiyanto <chandrahadiyantoalvin@gmail.com>
2022-12-26 08:00:04 +0700

    Java DAO

--- task3/dao/controller/BookController.java
+++ task3/dao/controller/BookController.java
@@ -14,11 +14,15 @@ public class BookController {
   static String opsi;
   static String judul;
   static String pembuat;
+  static Integer idbook;
 
   public static void menuApp() {
     System.out.println("=== SISTEM BUKU MANAGEMENT ===");
     System.out.println("1. Tambah Data Buku");
     System.out.println("2. Lihat Data Buku");
+    System.out.println("3. Hapus Data Buku");
+    System.out.println("4. Edit Data Buku");
+    System.out.println("5. Cari Data Buku Berdasar ID");
     System.out.print("Pilihan User: ");
     opsi = input.nextLine();
   }
@@ -29,7 +33,12 @@ public class BookController {
     Book book = new Book();
     book.setTitle("Buku 1");
     book.setAuthor("Penulis 1");
+    // save data
+    bookDao.save(book);
 
+    book = new Book();
+    book.setTitle("Buku 2");
+    book.setAuthor("Penulis 2");
     // save data
     bookDao.save(book);
 
@@ -54,7 +63,43 @@ public class BookController {
           break;
 
         case "2":
+          System.out.println("Semua Data Buku");
           System.out.println(bookDao.findAll());
+          break;
+
+        case "3":
+          System.out.println(bookDao.findAll());
+          System.out.println("Hapus Data Buku");
+          System.out.print("Pilihan ID Buku yang akan dihapus: ");
+          idbook = input.nextInt();
+          bookDao.delete(idbook);
+          System.out.println(bookDao.findAll());
+          break;
+
+        case "4":
+          System.out.println(bookDao.findAll());
+          System.out.println("Edit Data Buku");
+          System.out.print("Pilihan ID Buku yang akan diedit: ");
+          idbook = Integer.parseInt(input.nextLine());
+          System.out.print("Edit Judul Buku: ");
+          judul = input.nextLine();
+          System.out.print("Edit Pembuat Buku: ");
+          pembuat = input.nextLine();
+          
+          book = new Book();
+          book.setTitle(judul);
+          book.setAuthor(pembuat);
+          // save data
+          bookDao.update(book,idbook);
+          System.out.println(bookDao.findAll());
+          break;
+
+        case "5":
+          System.out.println(bookDao.findAll());
+          System.out.println("Edit Data Buku");
+          System.out.print("Pilihan ID Buku yang akan dicari: ");
+          idbook = Integer.parseInt(input.nextLine());
+          System.out.println(bookDao.findById(idbook));
           break;
 
         default:
